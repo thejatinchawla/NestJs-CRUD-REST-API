27b5dd1e90967e88fb6f3e6a7b756e4de9f2c61c
jatin chawla <jatinchawla682@gmail.com>
2024-03-23 13:25:46 +0530

    prisma config

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-
+import { User, Bookmark } from '@prisma/client';
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
