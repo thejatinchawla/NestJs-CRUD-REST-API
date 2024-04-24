420b87515721eaf404641044ca0def764ee861b3
jatin chawla <jatinchawla682@gmail.com>
2024-04-24 11:25:21 +0530

    http status codes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authservice: AuthService) {}
+  @HttpCode(HttpStatus.CREATED) // by default of post request will be 201.
   @Post('signup')
   signup(@Body() dto: AuthDto) {
     return this.authservice.signup(dto);
   }
+  @HttpCode(HttpStatus.OK)
   @Post('signin')
   signin(@Body() dto: AuthDto) {
     return this.authservice.signin(dto);
